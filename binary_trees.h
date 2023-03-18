fc8a9a314f57f52c3123bf47e596243018a668fd
tykoon787 <tykoon787@protonmail.com>
2023-03-18 09:07:48 +0300

    Updated Header File

---
+++ binary_trees.h
@@ -0,0 +1,23 @@
+#ifndef TREE_H
+#define TREE_H
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+struct binary_tree_s {
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+binary_tree_t *binary_tree_node(bianry_tree_t *parent, int value);
+
+#endif /* TREE_H */
