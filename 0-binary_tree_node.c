fdc053e427ba055024059b904639baab3545062c
tykoon787 <tykoon787@protonmail.com>
2023-03-18 09:45:59 +0300

    Updated code

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,8 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (new_node == NULL)
 		return (NULL);
-	new_node->parent = parent;
 	new_node->n = value;
+	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
 	return (new_node);
