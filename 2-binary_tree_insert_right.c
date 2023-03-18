aa94327b658dad43d0398864801e5dcd6468e4f3
tykoon787 <tykoon787@protonmail.com>
2023-03-18 10:12:39 +0300

    Updated code

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right- Function that inserts a node on the
+ * right side of a binary tree
+ * @parent: Parent node
+ * @value: value of the node
+ * Return: new_node
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    if (parent == NULL)
+        return NULL;
+
+    new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = parent->right;
+
+    if (parent->right != NULL)
+        parent->right->parent = new_node;
+
+    parent->right = new_node;
+
+    return new_node;
+}
